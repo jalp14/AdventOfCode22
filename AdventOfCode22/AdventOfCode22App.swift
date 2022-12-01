9bd47f9d994a836830edf250fc21b71f5454b58b
Jalp Desai <jalpdesai@icloud.com>
2022-12-01 09:39:55 +0000

    Initial Commit

---
+++ AdventOfCode22/AdventOfCode22App.swift
@@ -0,0 +1,17 @@
+//
+//  AdventOfCode22App.swift
+//  AdventOfCode22
+//
+//  Created by Jalp Desai on 01/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct AdventOfCode22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
