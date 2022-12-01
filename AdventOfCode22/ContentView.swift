32ed608b6fe9a5f868428bb855dc2305ae12ed7f
Jalp Desai <jalpdesai@icloud.com>
2022-12-01 10:34:31 +0000

    Initial Commit and Day 1

--- AdventOfCode22/ContentView.swift
+++ AdventOfCode22/ContentView.swift
@@ -7,6 +7,38 @@
 
 import SwiftUI
 
+final class Helper {
+    func getData(from file: String) -> String {
+        guard let path = Bundle.main.path(forResource: file, ofType: "txt") else { return "" }
+        guard let data = try? String(contentsOfFile: path, encoding: .utf8) else { return "" }
+        return data
+    }
+}
+
+
+class DaySolutions {
+    let helper = Helper()
+    
+    func day1() {
+        let input = helper.getData(from: "Day1")
+        var tempBois: [Int] = []
+        var counter = 0
+        
+        let calories = input.components(separatedBy: .whitespacesAndNewlines).compactMap { Int($0) ?? 0 }
+        
+        for calorie in calories {
+            if !(calorie == 0) {
+                counter += calorie
+            } else {
+                tempBois.append(counter)
+                counter = 0
+            }
+        }
+        print("Solution 1 -> \(tempBois.max())")
+        print("Solution 2 -> \(tempBois.sorted().suffix(3).reduce(0, +))")
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
@@ -15,12 +47,8 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
         }
-        .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        .onAppear() {
+            DaySolutions().day1()
+        }
     }
 }
